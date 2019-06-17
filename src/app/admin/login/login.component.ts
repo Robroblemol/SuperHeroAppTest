b2e0b099a6e47f5d70c3f99c7b976eb07a614fd2
Raziel <robroblemol>
2019-06-16 22:08:10 -0500

    adding validations login

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Auth } from 'src/app/auth/auth.service';
 import { Router } from '@angular/router';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,15 +9,26 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  createFormGroup (){
-    return new FormGroup(
-      {
-        email: new FormControl(''),
-        password: new FormControl(''),
-      }
-    )
-  }
-  loginForm: FormGroup;
+
+  emailPattern: any = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+
+  loginForm = new FormGroup(
+    {
+      email: new FormControl('',
+        [
+          Validators.required,
+          Validators.pattern(this.emailPattern),
+          Validators.minLength(5),
+        ]
+        ),
+      password: new FormControl('',
+        [
+          Validators.required,
+          Validators.minLength(5),
+        ]),
+    }
+  );
+
   constructor(
     public authService:Auth,
     public router: Router,
@@ -26,9 +37,25 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
 
   }
-  goRegister(){
-    this.router.navigate['register'];
-    return true;
+  signIn(){
+    if(this.loginForm.valid){
+      //console.log("login with",this.loginForm.value);
+      this.authService.signIn(
+        this.loginForm.value.email, 
+        this.loginForm.value.password
+      );
+    }else{
+      console.log("no pasé la validación");
+    }
+  }
+  get email(){//para poder commnicarse con el html
+    return this.loginForm.get('email');
+  }
+  get password(){//para poder commnicarse con el html
+    return this.loginForm.get('password');
+  }
+  onResetForm(){
+    this.loginForm.reset();
   }
 
 }
